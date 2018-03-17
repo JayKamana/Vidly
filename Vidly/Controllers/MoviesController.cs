4f9873422cc9b03b866fc71e3e980475c573fdca
James Kamana <jaykamana@gmail.com>
2018-03-18 00:42:48 +0300

    Add MemoryCache to Movie Controller.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -25,6 +26,7 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
+
             if (User.IsInRole(RoleName.CanManageMovies))
                 return View("List");
 
@@ -34,7 +36,12 @@ namespace Vidly.Controllers
         [Authorize (Roles = RoleName.CanManageMovies)]
         public ActionResult MovieForm()
         {
-            var genres = _context.Genres.ToList();
+            if (MemoryCache.Default["Genres"] == null)
+            {
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
+            }
+
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
 
             var viewModel = new MovieFormViewModel
             {
